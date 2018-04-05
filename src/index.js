7feb29d8a62d4990afb523178e25e92dae7d72ef
8Cookie9 <Cookie3141592653589>
2018-04-06 02:47:10 +0300

    fix1

--- src/index.js
+++ src/index.js
@@ -12,16 +12,7 @@ const config = require('./utils/config')
 const mongoose = require('mongoose')
 const poroutils = require('./utils/poroutils')
 
-mongoose
-  .connect(config.mongoUrl)
-  .then(() => {
-    console.log('connected to database', config.mongoUrl)
-  })
-  .catch(err => {
-    console.log(err)
-  })
 
-mongoose.Promise = global.Promise
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -266,9 +257,7 @@ server.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`)
 })
 
-server.on('close', () => {
-  mongoose.connection.close()
-})
+
 
 module.exports = {
   app, server
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const port = process.env.PORT
 const mongoUrl = process.env.MONGODB_URI
