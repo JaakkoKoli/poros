07cd3667624e18b0b8426e88a34b0d1e160dd0c8
8Cookie9 <Cookie3141592653589>
2018-04-06 13:15:35 +0300

    try n

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -260,16 +259,10 @@ app.get('/login', async (request, response) => {
   }
 })
 
-const server = http.createServer(app)
-
-server.listen(config.port, () => {
+app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`)
 })
 
-server.on('close', () => {
+app.on('close', () => {
   mongoose.connection.close()
 })
-
-module.exports = {
-  app, server
-}
