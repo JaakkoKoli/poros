9eb66e5ed2487a270abe97fb551c109ff1176e30
8Cookie9 <Cookie3141592653589>
2018-04-06 13:06:35 +0300

    try n

--- src/index.js
+++ src/index.js
@@ -259,16 +259,10 @@ app.get('/login', async (request, response) => {
   }
 })
 
-const server = http.createServer(app)
-
-server.listen(config.port, () => {
+app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`)
 })
 
 server.on('close', () => {
   mongoose.connection.close()
 })
-
-module.exports = {
-  app, server
-}
