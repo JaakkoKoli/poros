ab31ace34f878618488942fa06ba739b63b1029a
8Cookie9 <Cookie3141592653589>
2018-04-06 03:23:15 +0300

    node modules

--- src/models/achievement.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Achievement = mongoose.model('Achievement', {
-    name: String,
-    description: String,
-    users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-})
-
-module.exports = Achievement
--- src/models/attribute.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Attribute = mongoose.model('Attribute', {
-    name: String,
-    position: Number,
-    statchanges: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Statchange' }]
-})
-
-module.exports = Attribute
