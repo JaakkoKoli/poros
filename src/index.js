91546ae35e0ba847c974a0be9d13e04e6cc316b9
8Cookie9 <Cookie3141592653589>
2018-04-08 01:18:53 +0300

    set main poro

--- src/index.js
+++ src/index.js
@@ -281,6 +281,9 @@ app.get('/setmainporo', async (request, response) => {
       }
       var r = await axios.get('https://api.twitch.tv/kraken', conf)
       var currentUser = await User.findById(r.data.token.user_id)
+      console.log(currentUser)
+      console.log(currentUser.poros)
+      console.log(currentUser.poros.filter(x => x === id))
       if(currentUser.poros.filter(x => x === id).length>0){
         const u = await User.findByIdAndUpdate(currentUser._id, {$set: {mainporo: id}})
         response.send({user: u})
