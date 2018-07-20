0cca809197c8cd3ba2eaa30aed76623f63f9a369
8Cookie9 <jaakko.koli@gmail.com>
2018-07-20 04:24:30 +0300

    lll

--- src/index.js
+++ src/index.js
@@ -303,6 +303,7 @@ app.get('/login', async (request, response) => {
             .populate({ path: 'misc', populate: { path: 'statchange', model: StatChange } })
             .populate({ path: 'footwear', populate: { path: 'statchange', model: StatChange } })
             var token = generateToken()
+            console.log(removeTokens(user1))
             bcrypt.hash(token.toString(), 10, function(err, hash) {
               var session = Session({hash: hash, id: user1.userid, created: new Date()})
               session.save()
